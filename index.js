6407004e2ce79d898c3a1c17b911ccc945e75bd5
tengxiaoyang <656877449@qq.com>
2021-02-19 11:38:06 +0800

    add or modify

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req, res) => {
+  // 设置返回格式 JSON
   res.setHeader('Content-type', 'application/json')
 
-  const resData = {
-    name: 'txy',
-    site: 'blog',
-    env: process.env.NODE_ENV
+  // 处理blog路由
+  const blogData = handleBlogRouter(req, res)
+  if (blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
 
-  res.end(
-    JSON.stringify(resData)
-  )
+  // 处理user路由
+  const userData = handleUserRouter(req, res)
+  if (userData) {
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
+  }
+
+  // 未命中路由，返回404
+  res.writeHead(404, {"Content-type": "text/plain"})
+  res.write("404 Not Found\n")
+  res.end()
+
+  // const resData = {
+  //   name: 'txy',
+  //   site: 'blog',
+  //   env: process.env.NODE_ENV
+  // }
+
+  // res.end(
+  //   JSON.stringify(resData)
+  // )
+
+
 }
 
 module.exports = serverHandle
