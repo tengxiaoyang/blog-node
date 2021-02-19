495e436885de998b502b6096806db91d5911171d
tengxiaoyang <656877449@qq.com>
2021-02-19 13:59:27 +0800

    add or modify

--- src/router/user.js
+++ src/router/user.js
@@ -1,10 +1,10 @@
 const handleUserRouter = (req, res) => {
   const method = req.method //GET POST
-  const url = req.url
-  const path = url.split('?')[0]
+  // const url = req.url
+  // const path = url.split('?')[0]
 
   // 用户登录
-  if (method === 'POST' && path === '/api/user/login') {
+  if (method === 'POST' && req.path === '/api/user/login') {
     return {
       msg: '这是用户登录的接口'
     }
