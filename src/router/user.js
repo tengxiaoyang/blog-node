28d6b40f3530b68ba60b21cc949ce307174958c4
tengxiaoyang <656877449@qq.com>
2021-02-28 17:17:29 +0800

    add or modify

--- src/router/user.js
+++ src/router/user.js
@@ -18,7 +18,7 @@ const handleUserRouter = (req, res) => {
   if (method === 'POST' && req.path === '/api/user/login') {
     const { username, password } = req.body
     // const { username, password } = req.query
-    
+
     const result = login(username, password)
     return result.then(data => {
       if (data.username) {
@@ -38,7 +38,7 @@ const handleUserRouter = (req, res) => {
     })
   }
 
-  // // 登录验证的测试
+  // 登录验证的测试
   // if (method === 'GET' && req.path === '/api/user/login-test') {
   //   console.log('req.session is', req.session)
   //   console.log('req.session.username is', req.session.username)
@@ -53,16 +53,16 @@ const handleUserRouter = (req, res) => {
   //   }
   //   return Promise.resolve(new ErrorModel('尚未登录')) 
 
-    // if (req.cookie.username) {
-    //   return Promise.resolve(
-    //     new SuccessModel(
-    //       {
-    //         username: req.cookie.username
-    //       }
-    //     )
-    //   ) 
-    // }
-  }
+  //   // if (req.cookie.username) {
+  //   //   return Promise.resolve(
+  //   //     new SuccessModel(
+  //   //       {
+  //   //         username: req.cookie.username
+  //   //       }
+  //   //     )
+  //   //   ) 
+  //   // }
+  // }
 }
 
 module.exports = handleUserRouter
