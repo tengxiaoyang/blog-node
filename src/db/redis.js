0df43b264753e9fe0649762958a83c261813e41d
tengxiaoyang <656877449@qq.com>
2021-02-28 16:31:42 +0800

    add or modify

--- src/db/redis.js
+++ src/db/redis.js
@@ -8,13 +8,38 @@ redisClient.on_connect('error', err => {
 })
 
 function set(key, val) {
-
+  if (typeof val === 'object') {
+    val = JSON.stringify(val)
+  }
+  redisClient.set(key, val, redis.print)
 }
 
 function get(key) {
+  const promise = new Promise((resolve, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (val == null) {
+        resolve(null)
+        return
+      }
+      try {
+        resolve(
+          JSON.parse(val)
+        ) // 如果是JSON字符串就转成JSON对象
+      } catch (ex) {
+        resolve(val) // 如果try不成功，说明不是JSON字符串，直接返回
+      }
 
+      resolve(val)
+    })
+  })
+  return promise
 }
 
 module.exports = {
-
+  set,
+  get
 }
